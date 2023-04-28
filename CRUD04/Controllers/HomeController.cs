5eeda9f840659d45445c25386abbe8981897b28f
Galeote315 <galeote330@gmail.com>
2023-04-28 17:43:39 -0600

    Se agrego el servicio web  y se modifico el controler

--- CRUD04/Controllers/HomeController.cs
+++ CRUD04/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using CRUD03.Models;
+using ServiceReference1;
 
 namespace CRUD04.Controllers
 {
@@ -18,30 +19,62 @@ namespace CRUD04.Controllers
         {
             _logger = logger;
         }
+        public string EncodeStrToBase64(string valor)
+        {
+            byte[] myByte = System.Text.Encoding.UTF8.GetBytes(valor);
+            string myBase64 = Convert.ToBase64String(myByte);
+            return myBase64;
+        }
+        public static string DecodeBase64ToString(string valor)
+        {
+            byte[] myBase64ret = Convert.FromBase64String(valor);
+            string myStr = System.Text.Encoding.UTF8.GetString(myBase64ret);
+            return myStr;
+        }
+
 
         public IActionResult Index()
         {
-           
+            string x, y, z, p;
+            x = DecodeBase64ToString(string x);
+
+
 
         using (var contex = new CRUD03.Models.cer_addendasContext())
         {
-               
-                var b = contex.Addendas.Where(b => b.Estado == false);
                 
+                
+                var b = contex.Addendas.Where(b => b.Usuario == "jyyk");
 
-                
+
                 foreach (var item in b.ToList()) {
 
-                    Console.WriteLine(item.Estado = true);
+                    Console.WriteLine(item);
+                    MessageRequest input = new MessageRequest();
+                    input.operacion(x);
+                    input.rfcEmisor(y);
+                    input.xmlBase64(z);
+                    input.apiKey(p);
+
+                    var send = input.xmlBase64(input);
+                    WsFactClient ProcessDocument = new WsFactClient();
+                    var c = ProcessDocument.ProcessDocumentAsyn(send);
                     
+
+
                 }
+
                 ViewData["Usuario"] = b;
                 ViewBag.Usuario = b;
                 return View(ViewBag);
                 
             }
+
             
+
     }
+       
+
 
         public IActionResult Privacy()
         {
